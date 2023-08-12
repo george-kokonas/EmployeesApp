bc55b3339d2f33227b5956f290e61fc414e14b41
Iris <iriskalogirou1@gmail.com>
2023-08-12 19:55:21 +0300

    added .env

--- src/Maps/maps.js
+++ src/Maps/maps.js
@@ -3,7 +3,7 @@ import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 import records from "../records.json";
 function Maps() {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyCe2OPPDBp4vgb-cj3jGf8E31wnSREbuSE",
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
   });
 
   if (!isLoaded) return <div>Loading...</div>;
@@ -11,31 +11,6 @@ function Maps() {
 }
 function Map() {
   const center = useMemo(() => ({ lat: 37.98381, lng: 23.727539 }), []);
-
-  // const employees = [
-  //   {
-  //     id: 8,
-  //     name: "vc",
-  //     birth: "2023-08-24",
-  //     address: "Βυζαντίου 4, Παλλήνη, Ελλάδα",
-
-  //     lng: 23.8806118,
-  //     lat: 38.005209,
-
-  //     license: "YES",
-  //   },
-  //   {
-  //     id: 9,
-  //     name: "Damian",
-  //     birth: "2023-08-12",
-  //     address: "Athens, Ελλάδα",
-
-  //     lng: 23.7275388,
-  //     lat: 37.9838096,
-
-  //     license: "no",
-  //   },
-  // ];
 
   return (
     <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
